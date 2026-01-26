df5ad307958b1afc21e0c87a2f94575fcd04ffdf
PaulaSottomayor <paula.sottomayor@smc.pshs.edu.ph>
2026-01-26 22:45:04 +0800

    Add files via upload

---
+++ Sottomayor_Q3_FA02.py
@@ -0,0 +1,11 @@
+steps = [
+    [4500, 5200, 4800, 5000, 5300],  #Me
+    [4000, 4100, 3900, 4200, 4600],  #Lia
+    [6000, 5800, 5900, 6100, 6200]  #Jake
+]
+
+print("Jake's steps on Wednesday: ", steps[2][2])
+print("My steps... ", steps[0])
+steps[0][3] = 5500
+print("Updating my Thursday steps to 5500.")
+print("My updated steps: ", steps[0])
